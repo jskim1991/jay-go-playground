e108c986c104a0a097f505a3cdbcc3032a2190fc
Jay Kim <tensaijskim1991@gmail.com>
2024-02-03 10:04:46 +0900

    refactoring

--- concurrency/sample-v2.go
+++ concurrency/sample-v2.go
@@ -25,19 +25,65 @@ type aggregatedResultV2 struct {
 	B string
 }
 
-func v2GetResultA(ctx context.Context, input string) (string, error) {
+func (p *processorV2) launchV2(ctx context.Context, data requestV2) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		result, err := getResultAV2(ctx, data.A)
+		wg.Done()
+		if err != nil {
+			p.errs <- err
+			return
+		}
+		p.outA <- result
+	}()
+
+	go func() {
+		result, err := getResultBV2(ctx, data.B)
+		wg.Done()
+		if err != nil {
+			p.errs <- err
+			return
+		}
+		p.outB <- result
+	}()
+
+	wg.Wait()
+}
+
+func (p *processorV2) waitForABV2(ctx context.Context) (aggregatedResultV2, error) {
+	var input aggregatedResultV2
+	count := 0
+	for count < 2 {
+		select {
+		case a := <-p.outA:
+			input.A = a
+			count++
+		case b := <-p.outB:
+			input.B = b
+			count++
+		case err := <-p.errs:
+			return aggregatedResultV2{}, err
+		case <-ctx.Done():
+			return aggregatedResultV2{}, ctx.Err()
+		}
+	}
+	return input, nil
+}
+
+func getResultAV2(ctx context.Context, input string) (string, error) {
 	fmt.Println("Invoking A")
 	time.Sleep(1 * time.Second)
 	return "results from A", nil
 }
 
-func v2GetResultB(ctx context.Context, input string) (string, error) {
+func getResultBV2(ctx context.Context, input string) (string, error) {
 	fmt.Println("Invoking B")
 	time.Sleep(2 * time.Second)
 	return "results from B", nil
 }
 
-func v2GetResultC(ctx context.Context, input aggregatedResultV2) (string, error) {
+func getResultCV2(ctx context.Context, input aggregatedResultV2) (string, error) {
 	fmt.Println("Invoking C")
 	return "results from C", nil
 }
@@ -57,38 +103,15 @@ func main() {
 		A: "inputA",
 		B: "inputB",
 	}
+	p.launchV2(ctx, data)
 
-	var wg sync.WaitGroup
-	wg.Add(2)
-	go func() {
-		result, err := v2GetResultA(ctx, data.A)
-		wg.Done()
-		if err != nil {
-			p.errs <- err
-			return
-		}
-		p.outA <- result
-	}()
-
-	go func() {
-		result, err := v2GetResultB(ctx, data.B)
-		wg.Done()
-		if err != nil {
-			p.errs <- err
-			return
-		}
-		p.outB <- result
-	}()
-
-	wg.Wait()
-
-	ab := aggregatedResultV2{
-		A: <-p.outA,
-		B: <-p.outB,
+	ab, err := p.waitForABV2(ctx)
+	if err != nil {
+		panic(err)
 	}
 	fmt.Println("Gathered data from A and B:", ab)
 
-	c, err := v2GetResultC(ctx, ab)
+	c, err := getResultCV2(ctx, ab)
 	if err != nil {
 		p.errs <- err
 		return
